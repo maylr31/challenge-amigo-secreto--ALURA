d82a1976eb4e208d2c996fc54bac97082b604787
maylr31 <maylopes2122@gmail.com>
2025-02-04 00:34:45 -0300

    DESAFIO-AMIGO-SECRETO

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,67 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo');
+    if (amigo.value.trim() === '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
+    amigos.push(amigo.value);
+    atualizarListaAmigos();
+    amigo.value = '';
+}
+
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; // Limpa a lista antes de atualizar
+
+    amigos.forEach((amigo) => {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = amigo;
+        listaAmigos.appendChild(itemLista);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
+    embaralha(amigos);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = ''; // Limpa o resultado anterior
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigoSecreto;
+        if (i === amigos.length - 1) {
+            amigoSecreto = amigos[0]; // O último sorteia o primeiro
+        } else {
+            amigoSecreto = amigos[i + 1];
+        }
+
+        let itemResultado = document.createElement('li');
+        itemResultado.textContent = `${amigos[i]} --> ${amigoSecreto}`;
+        resultado.appendChild(itemResultado);
+    }
+}
+
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+}
